353c4859a5200ad374ed65d2d94a510287fae120
denny182 <denny.92@live.com>
2011-10-05 13:13:17 +0700

    TAMBAHAN PRAK3

---
+++ SESI 3/soal 3/belajar.java
@@ -0,0 +1,9 @@
+public class belajar{
+     public static void main (String[] args) {
+	      int ourfristnumber =10;
+		  double ourfristdouble =10;
+		  
+		  System.out.println(ourfristnumber);
+		  System.out.println(ourfristdouble);
+     }
+}	 
